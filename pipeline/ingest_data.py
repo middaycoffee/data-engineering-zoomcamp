ade9c6d1e24f3f33497e049eaf955789cbff4d6e
Barış A. <barris.aslan@gmail.com>
2026-01-26 16:32:01 +0000

    docker-compose integration

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,7 +1,7 @@
 # ingestion script for ingesting a data, NYC Taxi Data in our case, part by part with iteration. So both we can see
 # how the ingestion is going and don't process the big chunk of data at once.
 
-
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -33,19 +33,18 @@ parse_dates = [
 ]
 
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='nyc_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2019, type=int, help='Year of the data')
+@click.option('--month', default=10, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
-
-def run():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_port = 5432
-    pg_db = "nyc_taxi"
-
-    year = 2019
-    month = 10
-    table_name = 'yellow_taxi_data'
-
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunksize):
     prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz" 
     engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
@@ -55,21 +54,21 @@ def run():
         dtype=dtype,
         parse_dates=parse_dates,
         iterator = True,
-        chunksize = 100000
+        chunksize = chunksize
     )
 
     first_iter = True
     for df_chunks in tqdm(df_iter):
         if first_iter:
             df_chunks.head(0).to_sql(
-                name=table_name, 
+                name=target_table, 
                 con=engine, 
                 if_exists='replace'
             )
             first_iter = False
             
         df_chunks.to_sql(
-            name= table_name,
+            name= target_table,
             con=engine, 
             if_exists='append'
         )
@@ -77,4 +76,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
